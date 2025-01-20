1f9cd5845f6b05f9059a46e414d7e2ecb2c2610d
santiRosanova <rosanovasantiago@gmail.com>
2025-01-20 00:56:40 -0300

    Changes in confi files, npm, env variables and so on, to adapt it to new credentials and environments

--- trainmate/src/FirebaseConfig.ts
+++ trainmate/src/FirebaseConfig.ts
@@ -1,14 +1,15 @@
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs
 import { initializeApp } from "firebase/app";
-import { getAuth } from  "firebase/auth";
+import { getAuth } from "firebase/auth";
 
+// Load environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDIBZB99FwGwnaxcB_q93NoMqTeB0fmh4I",
-  authDomain: "trainmate-7ae2b.firebaseapp.com",
-  projectId: "trainmate-7ae2b",
-  storageBucket: "trainmate-7ae2b.appspot.com",
-  messagingSenderId: "64842776503",
-  appId: "1:64842776503:web:94ce0ce53673f2e6987232"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
